40f948b3fe7b75858877e8c2e104ad764c856f7d
Artem Tsolov <71283039+tsolovart@users.noreply.github.com>
2021-07-06 19:16:27 +0300

    Initial Commit

---
+++ mealTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mealTime
+//
+//  Created by Zaoksky on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
