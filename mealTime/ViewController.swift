29b19e041ef7eb78df70149edefc30fe3ff02d11
Artem Tsolov <71283039+tsolovart@users.noreply.github.com>
2021-07-06 19:38:30 +0300

    Initial Commit: Image / TableView / AddButton

--- mealTime/ViewController.swift
+++ mealTime/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.roundCornerView(corners: [.bottomLeft, .topLeft], radius: imageView.frame.size.height/2)
     }
 
+    @IBAction func addAction(_ sender: UIBarButtonItem) {
+        
+    }
+    
+}
 
+extension UIView {
+    // функция закругления углов
+    func roundCornerView(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: .init(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
 }
 
