6f5cd5310d94ac6851aa39bee47ce09269a5883d
Artem Tsolov <71283039+tsolovart@users.noreply.github.com>
2021-07-06 19:50:59 +0300

    added meal times

--- mealTime/ViewController.swift
+++ mealTime/ViewController.swift
@@ -7,18 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var tableView: UITableView!
     
+    var array = [Date]()
+    
+    // корректное отображение даты и времени
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.roundCornerView(corners: [.bottomLeft, .topLeft], radius: imageView.frame.size.height/2)
+        
+        // регистрация класса для ячеек
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    // заголовок секции
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "My meal time"
+    }
+    
+    // кол-во рядов = кол-ву элементов в массиве
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    // ячейка = время
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        let date = array[indexPath.row]
+        // конкретный прием прищи помещается в meal, если он есть
+        // извлечение даты
+        //guard let meal = person.meals?[indexPath.row] as? Meal, let mealDate = meal.date else { return cell! }
+        cell!.textLabel!.text = dateFormatter.string(from: date)
+        return cell!
     }
 
     @IBAction func addAction(_ sender: UIBarButtonItem) {
-        
+        let date = Date()
+        array.append(date)
+        tableView.reloadData()
     }
     
 }
